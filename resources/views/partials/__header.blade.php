59e25cd66066636ae7374875e357418d495fa3e9
GM Brhylle <gmjheizon027@gmail.com>
2024-05-28 20:44:43 +0800

    init

---
+++ resources/views/partials/__header.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing Page</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <style>
+        * {
+            padding: 0;
+            font-family: "Poppins", sans-serif;
+        }
+        main {
+            margin-top: 50%;
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    
