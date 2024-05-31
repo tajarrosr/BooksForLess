6285a9d112299bbfc55deb3fd14243e635659c8f
GM Brhylle <gmjheizon027@gmail.com>
2024-05-31 18:45:34 +0800

    pt. 2

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,11 +7,11 @@
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
         <div class="indicator">
 
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=""><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/></svg>          
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#efefef"><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/></svg>          
         <span class="badge badge-sm indicator-item bg-badge-800">8</span>
         </div>
       </div>
-      <div tabindex="0" class="mt-3 z-[1] text-green-600 card card-compact dropdown-content w-52 bg-base-100 shadow">
+      <div tabindex="0" class="mt-3 z-[1] text-text-500 card card-compact dropdown-content w-52 bg-base-100 shadow">
         <div class="card-body">
           <span class="font-bold text-lg">8 Items</span>
           <span class="text-info">Subtotal: $999</span>
--- resources/views/public/browse_books.blade.php
+++ resources/views/public/browse_books.blade.php
@@ -3,7 +3,7 @@
 
     <main class="container w-full transition duration-500 ease-in-out m-auto">
         <header>
-            <h1 class="uppercase text-4xl text-primary-200">browse books</h1>
+            <h1 class="uppercase text-4xl text-primary-300">browse books</h1>
             
             <div class="shopping">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/></svg>
@@ -14,7 +14,10 @@
         <div class="bookshelf-list"></div>
 
         <div class="card">
-            <h1 class="uppercase text-4xl text-primary-200
+            <h1 class="uppercase text-4xl text-primary-300">Card</h1>
+            <ul class="list-card"></ul>
+            <div class="check-out">
+                <div class="total">0</div>
                 <div class="close-shopping">close</div>
             </div>
         </div>
