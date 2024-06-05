89079b68ebb7a8b2341d778671bd83ceec7502e9
GM Brhylle <gmjheizon027@gmail.com>
2024-06-05 20:52:10 +0800

    fixed '+', '-', 'trash' post issues

--- resources/views/public/browse_books.blade.php
+++ resources/views/public/browse_books.blade.php
@@ -57,25 +57,25 @@
     </header>
 
         
-        {{-- * ADD TO CART SLIDER (LEFT) --}}
-    <div class="cart-container">
-        <div class="cart fixed top-0 left-full w-2/5 bg-background-100 border-l-2 border-solid border-accent-500 h-dvh transition-all shadow-2xl">
-            <h1 class="uppercase text-4xl text-text-900 m-0 ml-5 pt-0 pr-5 h-20 flex items-center font-butler">Bag</h1>
+                {{-- * ADD TO CART SLIDER (LEFT) --}}
+            <div class="cart-container">
+                <div class="cart fixed top-0 left-full w-2/5 bg-background-100 border-l-2 border-solid border-accent-500 h-dvh transition-all shadow-2xl">
+                    <h1 class="uppercase text-4xl text-text-900 m-0 ml-5 pt-0 pr-5 h-20 flex items-center font-butler">Bag</h1>
 
-            {{-- * FRAGMENTS --}}
-            <x-cart-card/>
+                    {{-- * FRAGMENTS --}}
+                    <x-cart-card/>
 
 
-            <div class="check-out-container border-solid border-t-2 border-accent-500 absolute bottom-0 w-full grid grid-cols-2 grid-rows-2 text-text-800 shadow-inner font-semibold">
-                <div class="total-label text-center capitalize item-1">Total:</div>
-                <div class="total item-2 text-center font-bold">₱0</div>
-                <div class="close-cart item-3 uppercase text-center cursor-pointer" onclick="toggleCartDrawer()">close</div>
-                <div class="check-out-button item-4 uppercase text-center">
-                    <button onclick="" class="uppercase">checkout</button></div>
-                {{-- cursor-pointer text-text-50 bg-primary-900 w-full h-16 flex items-center justify-center font-bold even:bg-gray-100 --}}
+                    <div class="check-out-container border-solid border-t-2 border-accent-500 absolute bottom-0 w-full grid grid-cols-2 grid-rows-2 text-text-800 shadow-inner font-semibold">
+                        <div class="total-label text-center capitalize item-1">Total:</div>
+                        <div class="total item-2 text-center font-bold">₱0</div>
+                        <div class="close-cart item-3 uppercase text-center cursor-pointer" onclick="toggleCartDrawer()">close</div>
+                        <div class="check-out-button item-4 uppercase text-center">
+                            <button onclick="" class="uppercase">checkout</button></div>
+                        {{-- cursor-pointer text-text-50 bg-primary-900 w-full h-16 flex items-center justify-center font-bold even:bg-gray-100 --}}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
     {{-- * ADD TO CART SLIDER (LEFT) --}}
     {{-- <x-cart-slider/> --}}
@@ -188,54 +188,35 @@
 
         const cartList = document.createElement('ul');
 
-    
-    // Iterate over the listCard array
-    listCard.forEach(book => {
-        // Create a new list item element
-        const listItem = document.createElement('li');
-        listItem.classList.add('grid', 'grid-cols-4', 'bg-accent-100', 'shadow-md', 'rounded-lg', 'p-2', 'w-11/12', 'mb-2', 'mx-auto', 'text-text-950', 'gap-1');
-        // Set the inner HTML of the list item with book details
-        listItem.innerHTML = `
-            <img src="${book.book_tmb}" alt="Book Cover" class="item-cover rounded-xl w-auto col-start-1 col-end-2 row-span-5">
+        listCard.forEach(book => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('grid', 'grid-cols-4', 'bg-accent-100', 'shadow-md', 'rounded-lg', 'p-2', 'w-11/12', 'mb-2', 'mx-auto', 'text-text-950', 'gap-1');
 
-            <span class="item-title text-xl font-bold col-start-2 col-end-5 row-span-2 font-merriweather">${book.book_title}</span>
+            listItem.innerHTML = `
+                <img src="${book.book_tmb}" alt="Book Cover" class="item-cover rounded-xl w-auto col-start-1 col-end-2 row-span-5">
+                <span class="item-title font-bold col-start-2 col-end-5 row-span-2">${book.book_title}</span>
+                <span class="item-author text-sm col-start-2 col-end-5 row-span-1">Author: ${book.book_author}</span>
+                <span class="item-price text-sm col-start-2 col-end-5 row-span-1">Unit Price: ${book.book_price}</span>
+                <div class="relative col-start-2 col-end-5">
+                    <span class="item-subtotal relative text-sm col-start-2 col-end-5">Subtotal: ${book.book_price * book.quantity}</span>
+                    <span class="item-quantity text-text-900 dark:text-text-950 text-3xl p-2 col-start-4 absolute col-end-5 right-1 bottom-0">×${book.quantity}</span>
+                </div>
+                <strong class="bg-badge-800 text-text-50 text-2xl font-bold dark:text-text-50 px-4 py-2 rounded hover:bg-primary-300 col-start-1 col-end-6 justify-center items-center" onclick="removeAll('${book.book_title}')">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" class="fill-text-50"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                </strong>
+                <button onclick="updateQuantity('${book.book_title}')" class="bg-success-700 text-text-50 text-2xl font-bold dark:text-text-50 px-4 py-2 rounded hover:bg-primary-300 col-start-5 row-start-1 row-span-3 w-full h-full">+</button>
+                <button onclick="removeOneFromCart('${book.book_title}')" class="bg-error-700 text-text-50 text-2xl font-bold dark:text-text-50 px-4 py-4 rounded hover:bg-primary-300 col-start-5 col-end-6 row-start-4 row-span-2 w-full h-full">-</button>
+            `;
+            cartList.appendChild(listItem);
 
-            <span class="item-author text-md col-start-2 col-end-5 row-span-1">Author: ${book.book_author}</span>
+            const subtotal = book.book_price * book.quantity;
 
-            <span class="item-price text-md col-start-2 col-end-5 row-span-1">Unit Price: ${book.book_price}</span>
-
-            <div class="relative col-start-2 col-end-5">
-            
-                <span class="item-subtotal relative text-md col-start-2 col-end-5 ">Subtotal: ${book.book_price * book.quantity}</span>
-        
-                <span class="item-quantity text-text-900 dark:text-text-950 text-3xl p-2 col-start-4 absolute col-end-5 right-1 bottom-0 font-merriweather">×${book.quantity}
-                </span>
-            
-            </div>
-
-           
-            <strong class="bg-badge-800 text-text-50 text-2xl font-bold dark:text-text-50 px-4 py-2 rounded hover:bg-primary-300  col-start-1 col-end-6 justify-center items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" class="fill-text-50"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></strong>
-
-            <button onclick="updateQuantity(${book.book_title})" class="bg-success-700 text-text-50 text-2xl font-bold dark:text-text-50 px-4 py-2 rounded hover:bg-primary-300 col-start-5 row-start-1 row-span-3 w-full h-full">+</button>
-
-            <button onclick="removeFromCart(${book.book_title})" class="bg-error-700 text-text-50 text-2xl font-bold  dark:text-text-50 px-4 py-4 rounded hover:bg-primary-300 col-start-5 col-end-6 row-start-4 row-span-2 w-full h-full">-</button>
-
-
-        `;
-        // Append the list item to the unordered list
-        cartList.appendChild(listItem);
-        
-        // Calculate subtotal for each book
-        const subtotal = book.book_price * book.quantity;
-        
-        // Add the subtotal to the subtotalForEachBook object
-        if (subtotalForEachBook.hasOwnProperty(book.book_title)) {
-            subtotalForEachBook[book.book_title] += subtotal;
-        } else {
-            subtotalForEachBook[book.book_title] = subtotal;
-        }
-    });
+            if (subtotalForEachBook.hasOwnProperty(book.book_title)) {
+                subtotalForEachBook[book.book_title] += subtotal;
+            } else {
+                subtotalForEachBook[book.book_title] = subtotal;
+            }
+        });
 
         total = Object.values(subtotalForEachBook).reduce((acc, curr) => acc + curr, 0);
         total = roundUp(total);
