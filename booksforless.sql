0a971093c7d4ef2412084c4b5f962f5d2445f7ad
Ritchmond James Tajarros <127801137+tajarrosr@users.noreply.github.com>
2024-06-07 09:45:03 +0800

    updated sql file

--- booksforless.sql
+++ booksforless.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2024 at 07:21 AM
+-- Generation Time: Jun 07, 2024 at 03:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Ritchmond James Tajarros', 'tajarrosritchmond2002@gmail.com', '$2y$12$9EFB5zlGmS8r0Mc7H/jcOOUuekiJFX9BLHqFqL9LKENA1odCus8yO', NULL, '2024-06-02 07:08:40', '2024-06-02 07:08:40');
+(1, 'Ritchmond James Tajarros', 'tajarrosritchmond2002@gmail.com', '$2y$12$eXkFLLK2mrX7htVC0VFICOj2OTzX2Tn.bBZ3xo47LEZYCQlUpXGJi', NULL, '2024-06-06 11:30:07', '2024-06-06 11:30:07');
 
 -- --------------------------------------------------------
 
@@ -56,13 +56,20 @@ CREATE TABLE `books` (
   `book_title` varchar(255) NOT NULL,
   `book_price` decimal(8,2) NOT NULL,
   `book_isbn` varchar(255) NOT NULL,
-  `book_stock` int(11) NOT NULL,
-  `book_genres` enum('Fantasy','Historical Fiction','Thriller','Romance','Science Fiction','Mystery','Poetry','Drama','Classics','Comedy/Humor','Religion/Spirituality','Philosophy','Psychology','Business','Young Adult','Children Literature','Memoir','Autobiography','Biography','Self-Help','Home & Garden','Western','Outdoors','Fairy Tale','Dystopian','Travel','Cookbooks','Action','Adventure','Horror','Military','Sports','History','Popular Science','Satire','Art','Health & Wellness','Mythic Fiction','Unidentified') NOT NULL,
+  `book_stock` int(11) NOT NULL DEFAULT 0,
+  `book_genres` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`book_genres`)),
   `book_desc` varchar(255) NOT NULL,
   `book_tmb` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `book_author`, `book_title`, `book_price`, `book_isbn`, `book_stock`, `book_genres`, `book_desc`, `book_tmb`, `created_at`, `updated_at`) VALUES
+(1, 'J.K. Rowling', 'Harry Potter and the Sorcerer\'s Stone', 500.00, '9780590353427', 100, '[\"Fantasy\",\"Young Adult\"]', 'The story follows a young boy named Harry Potter who discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry, where he uncovers the truth about his parents\' mysterious deaths and faces the dark wizard Voldemort.', 'pictures/az0cOm015X24ycmZoR8id7GmPDBrEsSNRK6iQ8Bk.jpg', '2024-06-06 12:08:41', '2024-06-06 12:08:41');
 
 -- --------------------------------------------------------
 
@@ -103,64 +110,6 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `genres`
---
-
-CREATE TABLE `genres` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `genres`
---
-
-INSERT INTO `genres` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Fantasy', NULL, NULL),
-(2, 'Historical Fiction', NULL, NULL),
-(3, 'Thriller', NULL, NULL),
-(4, 'Romance', NULL, NULL),
-(5, 'Science Fiction', NULL, NULL),
-(6, 'Mystery', NULL, NULL),
-(7, 'Poetry', NULL, NULL),
-(8, 'Drama', NULL, NULL),
-(9, 'Classics', NULL, NULL),
-(10, 'Comedy/Humor', NULL, NULL),
-(11, 'Religion/Spirituality', NULL, NULL),
-(12, 'Philosophy', NULL, NULL),
-(13, 'Psychology', NULL, NULL),
-(14, 'Business', NULL, NULL),
-(15, 'Young Adult', NULL, NULL),
-(16, 'Children Literature', NULL, NULL),
-(17, 'Memoir', NULL, NULL),
-(18, 'Autobiography', NULL, NULL),
-(19, 'Biography', NULL, NULL),
-(20, 'Self-Help', NULL, NULL),
-(21, 'Home & Garden', NULL, NULL),
-(22, 'Western', NULL, NULL),
-(23, 'Outdoors', NULL, NULL),
-(24, 'Fairy Tale', NULL, NULL),
-(25, 'Dystopian', NULL, NULL),
-(26, 'Travel', NULL, NULL),
-(27, 'Cookbooks', NULL, NULL),
-(28, 'Action', NULL, NULL),
-(29, 'Adventure', NULL, NULL),
-(30, 'Horror', NULL, NULL),
-(31, 'Military', NULL, NULL),
-(32, 'Sports', NULL, NULL),
-(33, 'History', NULL, NULL),
-(34, 'Popular Science', NULL, NULL),
-(35, 'Satire', NULL, NULL),
-(36, 'Art', NULL, NULL),
-(37, 'Health & Wellness', NULL, NULL),
-(38, 'Mythic Fiction', NULL, NULL),
-(39, 'Unidentified', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -218,8 +167,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2024_05_28_150649_create_admins_table', 1),
-(5, '2024_05_29_131425_create_books_table', 1),
-(6, '2024_06_05_044139_create_genres_and_book_genre_tables', 2);
+(5, '2024_05_29_131425_create_books_table', 1);
 
 -- --------------------------------------------------------
 
@@ -253,7 +201,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('hHAcp0coA8q9ddXtHEtUBPP1R6Oib6pv0jiq2YOr', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiR2xpcjhVT3lRbjRLaFdEV1JMMGc0RzJlWmRXZXFkNnM5MlpBTXk1NyI7czozOiJ1cmwiO2E6MDp7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjQ0OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYWRtaW4vaW52ZW50b3J5L2NyZWF0ZSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTI6ImxvZ2luX2FkbWluXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1717564723);
+('hxkikAgjE2ze651AM1qbF6mZMqNvJrgir22SKas3', 3, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiZDNPNHNSYVlkQkNWbG45bXhTdGNjOWM5NkZrRExnQUNXem1ETDJkVyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzQ6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9icm93c2UtYm9va3MiO31zOjUyOiJsb2dpbl9hZG1pbl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mzt9', 1717710549),
+('ZBYfpaNFlEuc7PyvQlrQBy5412cvJ7Os2hxFeicV', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiNG9NejNveGxLNjhOMWlXTzMyakExQzJlejRsalQxbmJCb0lTaVBTUyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9jaGVja291dCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1717722809);
 
 -- --------------------------------------------------------
 
@@ -263,14 +212,23 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) NOT NULL,
+  `picture` varchar(255) DEFAULT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `picture`, `remember_token`, `created_at`, `updated_at`) VALUES
+(3, 'Lebron', 'James', 'lebronjames', 'lebronjames@gmail.com', '$2y$12$x2qqRZfVjEmOUMjZHbyraOUC1nfc3t3kMyLMg46fah5K7mawzL7Pu', 'assets/images/profile/1717704056.jpeg', NULL, '2024-06-06 12:00:56', '2024-06-06 12:00:56');
 
 --
 -- Indexes for dumped tables
@@ -307,12 +265,6 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `genres`
---
-ALTER TABLE `genres`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `jobs`
@@ -352,6 +304,7 @@ ALTER TABLE `sessions`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
@@ -362,25 +315,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `genres`
---
-ALTER TABLE `genres`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -392,13 +339,13 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
